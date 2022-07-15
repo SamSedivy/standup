4105ed46746bd2b83da2cf90720ea01580cdbd84
tucanSam <sls325@pitt.edu>
2022-07-15 10:29:56 -0400

    Init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Wheel from './components/wheel';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import './styles.css';
+
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.people = ['Karl', 'Jes', 'Sam', 'Derek', 'Charles', 'John', 'Allan', 'Max', 'Zhou'];
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Who is next?</h1>
+                <Wheel items={this.people} />
+            </div>
+        );
+    }
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
